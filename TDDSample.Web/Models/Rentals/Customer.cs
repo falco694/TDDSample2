ae985377c26b3833db6057922b138b440d3d259d
s.funakura <s.funakura@genbasupport.com>
2018-03-03 15:41:47 +0900

    テストケースを追加

--- TDDSample.Web/Models/Rentals/Customer.cs
+++ TDDSample.Web/Models/Rentals/Customer.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TDDSample.Web.Models.Rentals
 {
-    public sealed class Customer{
+    public sealed class Customer
+    {
         private readonly List<Rental> _rentals = new List<Rental>();
 
         public string Name { get; }
@@ -16,13 +18,41 @@ namespace TDDSample.Web.Models.Rentals
         {
             _rentals.Add(rental);
         }
-        
+
         /**
          * レンタル金額を円で返す。
          */
         public string Statement()
         {
-            return null;
+            var fee = RentelFee();
+            return $"{fee:N0}円";
+            // return "300円";
+        }
+
+        public int RentelFee()
+        {
+            var fee = 0;
+            foreach (var rental in _rentals)
+            {
+                fee += RentelFee(rental);
+            }
+
+            return fee;
+        }
+
+        private static int RentelFee(Rental rental)
+        {
+            if (rental.Movie.RentalType == MovieRentalType.NewRelease)
+            {
+                return rental.DaysRented * 300;
+            }
+            
+            if (rental.Movie.RentalType == MovieRentalType.Regular)
+            {
+                return 200;
+            }
+
+            throw  new InvalidOperationException();
         }
     }
 }

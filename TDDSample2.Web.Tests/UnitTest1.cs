9af5d3a0fa4d876cd240faa94ab3b9d627bc30f4
s.funakura <s.funakura@genbasupport.com>
2018-03-03 14:10:53 +0900

    テストプロジェクト追加

---
+++ TDDSample2.Web.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
+namespace TDDSample2.Web.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+
+           [Fact]
+        public void Test2()
+        {
+            Assert.Equal(2.0m, Math.Round(1.5m));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal(2.0m, Math.Round(2.5m));
+        }
+    }
+}
